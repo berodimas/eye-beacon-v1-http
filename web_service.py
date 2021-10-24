3f92e996056bb9ffe5b25dce6de29c3c814ad891
Dimas Adrian Mukti <dadrianm25@gmail.com>
2021-10-24 20:37:14 +0700

    update scripts

--- web_service.py
+++ web_service.py
@@ -5,15 +5,16 @@ import redis, json, argparse
 app = Flask(__name__)
 api = Api(app)
 
-people_counter = {
+counter = {
     'enter': 0,
     'exit': 0,
     'total': 0
 }
 
-people_status = {
+status = {
     'name': '',
-    'isInside': False
+    'isInside': False,
+    'room': ''
 }
 
 ap = argparse.ArgumentParser()
@@ -27,9 +28,9 @@ args = vars(ap.parse_args())
 
 r = redis.Redis(host=args["redis"], port=6379, db=0)
 
-@app.route("/eyebeacon/dashboard/people_counter", methods=['POST'])
-def post():
-    if len(people_counter) == 0:
+@app.route("/eyebeacon/dashboard/counter", methods=['POST'])
+def handler_counter():
+    if len(counter) == 0:
         abort(404)
     if not request.json:
         abort(400)
@@ -39,19 +40,19 @@ def post():
         abort(400)
     if 'total' in request.json and type(request.json['total']) is not int:
         abort(400)
-    people_counter['enter'] = request.json.get(
-        'enter', people_counter['enter'])
-    people_counter['exit'] = request.json.get(
-        'exit', people_counter['exit'])
-    people_counter['total'] = request.json.get(
-        'total', people_counter['total'])
-    json_1 = json.dumps(people_counter)
-    r.publish('http_counter', json_1)
-    return jsonify({'people_counter': people_counter})
+    counter['enter'] = request.json.get(
+        'enter', counter['enter'])
+    counter['exit'] = request.json.get(
+        'exit', counter['exit'])
+    counter['total'] = request.json.get(
+        'total', counter['total'])
+    responese = json.dumps(counter)
+    r.publish('http_counter', responese)
+    return jsonify({'counter': counter})
 
-@app.route("/eyebeacon/dashboard/people_status", methods=['GET'])
-def get():
-    if len(people_status) == 0:
+@app.route("/eyebeacon/dashboard/status/<room>", methods=['POST'])
+def handler_status(room):
+    if len(status(room)) == 0:
         abort(404)
     if not request.json:
         abort(400)
@@ -59,14 +60,14 @@ def get():
         abort(400)
     if 'isInside' in request.json and type(request.json['isInside']) is not bool:
         abort(400)
-    people_status['name'] = request.json.get(
-        'name', people_status['name'])
-    people_status['isInside'] = request.json.get(
-        'isInside', people_status['isInside'])
-    json_2 = json.dumps(people_status)
-    r.publish('http_status', json_2)
-    return jsonify({'people_status': people_status})
-
+    status['name'] = request.json.get(
+        'name', status['name'])
+    status['isInside'] = request.json.get(
+        'isInside', status['isInside'])
+    status['room'] = room
+    responese = json.dumps(status)
+    r.publish('http_status', responese)
+    return jsonify({'status': status})
 
 if __name__ == "__main__":
     app.run(host=args["url"], port=args["port"])
